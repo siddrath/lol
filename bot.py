a3cb8e7d848e1b86591a384aa30238aed4493eae
siddrath <40337840+siddrath@users.noreply.github.com>
2018-11-09 18:59:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import re
 import requests
 import asyncio
 import random
+import psutil
 from discord.ext import commands
 from PIL import Image, ImageDraw, ImageFont
 import json
@@ -640,7 +641,7 @@ async def dog(ctx):
  
 @bot.command()
 async def neko(ctx):
-    ''''sends cute dog pics'''
+    ''''sends cute nekos'''
     r = requests.get("https://nekos.life/api/neko").json()
 
     colours = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a]
