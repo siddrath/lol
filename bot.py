217e593ba11c1b50f3db19fa1128c86212456162
siddrath <40337840+siddrath@users.noreply.github.com>
2018-12-05 22:20:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -850,19 +850,7 @@ async def help(ctx):
     
     
     
-@bot.command(hidden=True)
-async def reload(ctx, extension):
-    if ctx.author.id == 411496838550781972:
-        try:
-           bot.unload_extension(extension)
-            bot.load_extension(extension)
-            embed = discord.Embed(title="Reload", description=f'''Reloaded {extension}''',
-                                  color=discord.Colour.dark_purple())
-            await ctx.send(embed=embed)
-       except ModuleNotFoundError:
-            await ctx.send("```No such extention exists```")
-    else:
-        await ctx.send("```You can't do it buddy you better know it```")
+
         
 
 @bot.listen()
