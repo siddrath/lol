394d101b2179ad64ad8fab5c24a6d517e83c1a60
siddrath <40337840+siddrath@users.noreply.github.com>
2018-12-05 22:17:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -853,17 +853,14 @@ async def help(ctx):
 @bot.command(hidden=True)
 async def reload(ctx, extension):
     if ctx.author.id == 411496838550781972:
-       try:
-            bot.unload_extension(extension)
-            bot.load_extension(extension)
-            embed = discord.Embed(title="Reload", description=f'''Reloaded {extension}''',
-                                  color=discord.Colour.dark_purple())
-            await ctx.send(embed=embed)
-       except ModuleNotFoundError:
-            await ctx.send("```No such extention exists```")
+        try:
+            bot.unload_extension(module)
+            bot.load_extension(module)
+    except Exception as e:
+        await ctx.send('\N{PISTOL}')
+        await ctx.send('{}: {}'.format(type(e).__name__, e))
     else:
-        await ctx.send("```You can't do it buddy you better know it```")
-    
+        await ctx.send('\N{OK HAND SIGN}')
 
         
 
