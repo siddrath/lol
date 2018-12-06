01f9e19b0a345788220ffdcaea33d08e54d36e85
siddrath <40337840+siddrath@users.noreply.github.com>
2018-12-06 21:32:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -931,7 +931,6 @@ async def change_nickname(ctx, *, name: str = None):
         
 @bot.command()
 async def uptime(ctx):
-    time = json.load(open('db/uptime.json', "r"))['uptimestats']
     uptimeraw = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
     uptime = datetime.datetime.utcnow() - uptimeraw
     hours, remainder = divmod(int(uptime.total_seconds()), 3600)
