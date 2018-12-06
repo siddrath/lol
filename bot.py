5ba706b3d09374c8ffb481f47c7966a063349f92
siddrath <40337840+siddrath@users.noreply.github.com>
2018-12-06 20:26:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -916,14 +916,19 @@ async def change_username(ctx, *,name: str):
 @bot.command(name="nickname")
 async def change_nickname(ctx, *, name: str = None):
     """ Change nickname. """
-    try:
-        await ctx.guild.me.edit(nick=name)
-        if name:
-            await ctx.send(f"Successfully changed nickname to **{name}**")
-        else:
-            await ctx.send("Successfully removed nickname")
-    except Exception as err:
-        await ctx.send(err)
+    if ctx.message.author.guild_permissions.administrator or ctx.message.author.id == '411496838550781972':
+        try:
+            await ctx.guild.me.edit(nick=name)
+            if name:
+
+                await ctx.send(f"Successfully changed nickname to **{name}**")
+            else:
+                await ctx.send("Successfully removed nickname")
+   
+
+        except Exception as err:
+            await ctx.send(err)
+
         
 @bot.command(pass_context=True)
 async def help(ctx):
