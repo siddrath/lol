3034121bb7549ed12a8d69cad6e2aa1a21d0929c
siddrath <40337840+siddrath@users.noreply.github.com>
2018-12-13 11:50:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -445,25 +445,33 @@ async def wikipedia(ctx, *, query: str):
         await ctx.send('```{}```'.format(message))
         
             
-@bot.command(pass_context=True, aliases=['whois'])
-async def userinfo(ctx, member: discord.Member = None):
-
-    name="user",
-    if member is None:
-        member = ctx.author
-
-    e = discord.Embed(title=f"User: {member.name}",description=f"This is all the information I could find on {member.name}...",)
-    e.set_thumbnail(url=member.avatar_url_as(static_format="png"))
-    e.add_field(name="Name",value=member.name)
-    e.add_field(name="Discriminator",value=f"#{member.discriminator}")
-    e.add_field(name="ID",value=str(member.id))
-    e.add_field(name="Bot",value=str(member.bot).capitalize())
-    e.add_field(name="Highest Role",value=member.top_role.mention)
-    e.add_field(name="Join Position",value=f"#{sorted(member.guild.members, key=lambda m: m.joined_at).index(member) + 1}")
-    e.add_field(name="Created Account",value=member.created_at.strftime("%c"))
-    e.add_field(name="Joined This Server",value=member.joined_at.strftime("%c"))
-    e.add_field(name="Roles",value=f"{len(member.roles)-1} Roles: {', '.join([r.mention for r in member.roles if not r.is_default()])}")
-    await ctx.send(embed=e)
+@bot.command(aliases=['whois'])
+async def userinfo(ctx, *, member: discord.Member = None):
+    """
+    Retrieve information about a member of the guild.
+    If no arguments are passed, information about the author is used.
+    """
+    async with ctx.typing():
+        member = member or ctx.author
+        icon_url = member.avatar_url_as(static_format='png')
+        e = discord.Embed(type='rich', color=member.color)
+        e.set_thumbnail(url=icon_url)
+        e.add_field(name='Name', value=str(member))
+        e.add_field(name='ID', value=member.id)
+        e.add_field(name='Nickname', value=member.nick)
+        e.add_field(name='Bot Created' if member.bot else 'User Joined Discord', value=member.created_at.strftime(datetime_format))
+        e.add_field(name='Joined Guild', value=member.joined_at.strftime(datetime_format))
+        e.add_field(name="Highest Role",value=member.top_role.mention)
+        e.add_field(name="Join Position",value=f"#{sorted(member.guild.members, key=lambda m: m.joined_at).index(member) + 1}")
+            
+        e.add_field(name='Status and Activity', value='{}, {}'.format(
+            'DND' if member.status is discord.Status.dnd else str(member.status).title(),
+            'no activity' if member.activity is None else '{} {}'.format('listening to' if member.activity.type == discord.ActivityType.listening else member.activity.type.name, member.activity.name)
+        ), inline=False)
+            
+        e.add_field(name='Roles', value=', '.join(role.name for role in reversed(member.roles[1:])) or 'None', inline=False)
+        e.add_field(name='Icon URL', value=icon_url, inline=False)
+        await ctx.send(embed=e)
     
 
 @bot.command()
